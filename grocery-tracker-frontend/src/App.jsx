accf24120e5c54ef5f6c08faff0819d0340b1d0c
ericgajdusek <115500057+ericgajdusek@users.noreply.github.com>
2025-05-18 00:35:11 -0700

    updated app

--- grocery-tracker-frontend/src/App.jsx
+++ grocery-tracker-frontend/src/App.jsx
@@ -3,7 +3,16 @@ import "./App.css";
 import ItemForm from "./components/ItemForm";
 
 function App() {
-  return <></>;
+  const handleAddItem = (item) => {
+    console.log("New item: ", item);
+  };
+
+  return (
+    <div className="max-wd-m mx-auto mt-10">
+      <h1 className="text-2xl font-bold mb-4">Grocery Item Entry</h1>
+      <ItemForm onSubmit={handleAddItem} />
+    </div>
+  );
 }
 
 export default App;
